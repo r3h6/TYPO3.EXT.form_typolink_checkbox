6836e05ef0e0048f0a3e858a3a85ce94ae0c2fd8
r3h6 <r3h6@outlook.com>
2025-01-23 20:04:55 +0100

    [TASK] Make extension compatible with TYPO3 v13

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -5,7 +5,7 @@ cd "$THIS_SCRIPT_DIR" || exit 1
 cd ../Docker || exit 1
 
 export PHP=${PHP:-8.2}
-export DOCKER_PHP_IMAGE=`echo "typo3/core-testing-php${PHP}" | sed -e 's/\.//'`
+export DOCKER_PHP_IMAGE="ghcr.io/typo3/core-testing-php$(echo "${PHP}" | tr -d '.')"
 export ROOT_DIR=`readlink -f ${PWD}/../../`
 export HOST_UID=`id -u`
 
@@ -14,17 +14,17 @@ ARGS="${@:2}"
 
 case $SUITE in
     composer)
-        docker-compose run composer $ARGS
+        docker compose run composer $ARGS
     ;;
     unit)
         ARGS=${ARGS:-Tests/Unit/}
-        docker-compose run unit -c .Build/vendor/typo3/testing-framework/Resources/Core/Build/UnitTests.xml $ARGS
+        docker compose run unit -c .Build/vendor/typo3/testing-framework/Resources/Core/Build/UnitTests.xml $ARGS
     ;;
     functional)
         rm -rf .Build/web/typo3temp/var/tests/
         ARGS=${ARGS:-Tests/Functional/}
         ARGS="-c .Build/vendor/typo3/testing-framework/Resources/Core/Build/FunctionalTests.xml $ARGS"
-        docker-compose run functional $ARGS
+        docker compose run functional $ARGS
     ;;
     *)
     echo "Invalid argument '$SUITE'"
@@ -33,5 +33,5 @@ case $SUITE in
 esac
 
 SUITE_EXIT_CODE=$?
-docker-compose down --remove-orphans
+docker compose down --remove-orphans
 exit $SUITE_EXIT_CODE
---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dependencies' => ['form'],
+    'imports' => [
+        '@r3h6/form-typolink-checkbox/' => 'EXT:form_typolink_checkbox/Resources/Public/JavaScript/',
+    ],
+];
